8c308da710d5b8ea57f28a28230770ed5a1ca860
Caroline-Wacera <carolinewacera4@gmail.com>
2025-12-03 21:47:37 +0300

    Day_3 conditional statements and for loops exercises

---
+++ Day_3/for_loops.py
@@ -0,0 +1,27 @@
+fruits = ["apple", "banana", "mango"]
+for fruit in fruits:
+    print(fruit)
+
+word = "Yellow"
+for letter in word:
+    print(letter)
+
+#Create a list of 4 colors.
+#Use a for loop to print each color.
+colors = ["yellow" ,"red" ,"pink", "Blue"]
+for color in colors:
+    print(color)
+
+
+#Use range() to print numbers from 1 to 5.
+for i in range(6):
+    print(i)
+
+
+#Create a list of 3 names.
+names = ["Glory", "Shiru", "Njeri"]
+for name in names:
+    print(name)
+#Use a for loop to print "Hello, <name>!" for each name.
+for name in names:
+    print(f"Hello, {name}")
