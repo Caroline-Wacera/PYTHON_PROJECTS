b3a271f250a8963cd5fd9283d318555b994b109b
Caroline-Wacera <carolinewacera4@gmail.com>
2025-12-21 22:58:26 +0300

    More practice exercises on uses of count in while loops

--- Day_4/count_uses_1.py
+++ Day_4/count_uses_1.py
@@ -5,7 +5,6 @@ If correct → print "Correct"
 If failed → "Out of attempts"""
 attempts = 0
 success = False
-
 while attempts < 2:
     guess = input("Guess an input: ")
     if guess == "yes":
@@ -13,7 +12,6 @@ while attempts < 2:
         success = True
         break
     attempts += 1
-
 if not success:
     print("Out of attempts")
 
@@ -26,10 +24,8 @@ Wrong → "Wrong PIN"
 After 3 failures → "Card blocked"""
 attempts = 0
 correct = False
-
 while attempts < 3:
     pin = input("Enter the pin: ")
-    
     if pin == "4321":
         print("Transaction allowed")
         correct = True
@@ -40,3 +36,63 @@ while attempts < 3:
     attempts += 1
 if not correct:
     print("Card blocked")
+
+
+"""🟠 Practice 3 (Medium)
+Ask for password "admin123"
+Rules:
+3 attempts
+Print "Attempts left: X" after each failure
+Lock after 3"""
+attempts = 0
+while attempts < 3:
+    password = input("Enter a password:")
+    if password == "admin123":
+        print("Correct password")
+        break
+    else:
+        print("Attempts left:", 3 - attempts)
+    attempts += 1
+if attempts == 3:
+    print("Locked")
+
+
+"""🔴 Practice 4 (Real-World)
+Mpesa-style PIN:
+Rules:
+PIN = "1111"
+Max attempts = 3
+If correct → stop immediately
+If wrong → count attempts
+After lock → "Try again after 24 hours"""
+attempts = 0
+while attempts < 3:
+    pin = input("Enter your PIN: ")
+    if pin == "1111":
+        print("PIN accepted")
+        break
+    else:
+        attempts += 1
+if attempts == 3:
+    print("Try again after 24 hours")
+
+
+"""🔥 Practice 5 (Challenge)
+Admin login:
+Conditions:
+Username must be "admin"
+Password must be "root123"
+3 attempts TOTAL
+Count failed attempts (username OR password)"""
+attempts = 0
+while attempts < 3:
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    if username == "admin" and password == "root123":
+        print("Login successful")
+        break
+    else:
+        attempts += 1
+        print("Wrong credentials")
+if attempts == 3:
+    print("Account locked")
