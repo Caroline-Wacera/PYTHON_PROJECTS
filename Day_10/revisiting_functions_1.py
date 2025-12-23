96f64e8db4d531256083169f834e1989bd35185f
Caroline-Wacera <carolinewacera4@gmail.com>
2025-12-23 23:50:41 +0300

    More exercises on basic functions

---
+++ Day_10/revisiting_functions_1.py
@@ -0,0 +1,35 @@
+"""👉 Write a function called greet
+👉 It should print "Welcome!"
+👉 Call the function once"""
+def greet():
+    print("Welcome")
+greet()
+
+
+"""👉 Write a function called ask_name
+👉 Ask the user for their name
+👉 Print: "Hello, NAME"""
+def ask_name():
+    name = input("Enter your name:")
+    print("Hello",name)
+ask_name()
+
+
+"""👉 Write a function called triple
+👉 It takes one number
+👉 Prints the number multiplied by 3
+👉 Call it with 5"""
+def triple(number):
+    print(number * 3)
+triple(5)
+
+
+"""👉 Write a function called cube
+👉 Takes one number
+👉 Returns the number cubed
+👉 Print the result
+📌 Formula reminder:
+cube = number × number × number"""
+def cube(number):
+    return number * number * number
+result 
